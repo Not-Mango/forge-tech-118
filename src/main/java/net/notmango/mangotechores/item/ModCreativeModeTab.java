0c9e8d3242cc67550c0189958bc1aa8ece328394
Not-Mango <jackhb@hotmail.co.uk>
2022-08-21 14:25:42 +0100

    Custom Creative Tabs

---
+++ src/main/java/net/notmango/mangotechores/item/ModCreativeModeTab.java
@@ -0,0 +1,22 @@
+package net.notmango.mangotechores.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.notmango.mangotechores.block.ModBlocks;
+
+public class ModCreativeModeTab {
+
+    public static final CreativeModeTab MANGO_ORES = new CreativeModeTab("mangotechores") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.TIN_INGOT.get());
+        }
+    };
+
+    public static final CreativeModeTab MANGO_BLOCKS = new CreativeModeTab("mangotechblocks") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModBlocks.TIN_BLOCK.get());
+        }
+    };
+}
